3552eddc3215ad1d4da494fe8b49202b549b2e53
Hat1ko <pasha1pple@gmail.com>
2019-12-29 13:03:24 +0200

    initial commit

---
+++ AnotherMineSweeperTests/AnotherMineSweeperTests.swift
@@ -0,0 +1,34 @@
+//
+//  AnotherMineSweeperTests.swift
+//  AnotherMineSweeperTests
+//
+//  Created by Pasha Suprun on 29.12.2019.
+//  Copyright © 2019 Pasha Suprun. All rights reserved.
+//
+
+import XCTest
+@testable import AnotherMineSweeper
+
+class AnotherMineSweeperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
